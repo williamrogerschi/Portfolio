dd886e967c5f430be4d1b596be3b400c9cb62b57
Billy Rogers <william.rogers.chi@gmail.com>
2023-12-18 15:34:45 -0600

    working on about me page

--- billy-port/src/components/About.jsx
+++ billy-port/src/components/About.jsx
@@ -1,4 +1,48 @@
+import HomeNav from './HomeNav'
+import './about.css'
+
 const About = () => {
-    return <h2>This is my about me page!</h2>
+    return (
+        <>
+            <HomeNav />
+            <div className="about-wrapper">
+                <div className="about-content">
+                    <div className="about-content-container">
+                        <h1 className="about">About</h1>
+                        <h3 className="about-h3">Billy Rogers, a creative mind.</h3>
+                    </div>
+                    <div className='experience-wrapper'>
+                        <div className='experience'>
+                            <p className='experience-header'>Experience</p>
+                        </div>
+                        <div className='experience-content'>
+                            <span className='about-exp'>- General Assembly | </span> <span className='about-exp'>September 2023 - December 2023 | Remote</span>
+                            <p className='content-exp'>- Certificate of completion for Software Engineering Immersive 12 week bootcamp.</p>
+                            <p className='content-exp'>- Degree in Business Administration from Northeastern Illinois University | Graduated May 2021</p>
+                        </div>
+                    </div>
+
+                    <div className='resume-btn-wrapper'>
+                        <a href='/Resume' type='button' className='resume-btn'>Resume</a>
+                    </div>
+
+                    <div className='experience-wrapper'>
+                        <div className='experience'>
+                            <p className='experience-header'>Skills</p>
+                        </div>
+                        <div className='experience-content'>
+                            <span className='about-exp'>- Languages: </span> <span className='about-exp'>JavaScript ·  HTML · CSS · Python · SQL ·  Mongo · JSON | Database - MongoDB</span>
+                            <p className='content-exp'>- Libraries and Frameworks: React · Express.js · Django  ·  Bootstrap  ·  Tailwind</p>
+                            <p className='content-exp'>- Other: RESTful Routing · JSON API  ·  SAP · Salesforce</p>
+                        </div>
+                    </div>
+
+                    <div className='resume-btn-wrapper'>
+                        <a href='/Contact' button type='button' className='contact-btn'>Contact</a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 export default About
--- billy-port/src/components/Projects.jsx
+++ billy-port/src/components/Projects.jsx
@@ -10,7 +10,6 @@ const Projects = () => {
     <HomeNav />
             <div className="work-wrapper">
                 <div className="work-content">
-                    <div className="work-content-container">
                         <h1>Projects</h1>
                         <p className="work-p">
                             "Step into the innovative world of my Software Engineering Immersive journey, where creativity met code! Throughout this transformative bootcamp, I navigated the intricacies of software development, honing my skills to craft diverse and impactful projects. From designing responsive web applications that streamline user experiences to diving deep into backend development for robust functionality, each project reflects my dedication to merging cutting-edge technology with intuitive design. With a focus on collaborative teamwork, problem-solving, and a passion for delivering scalable solutions, my portfolio showcases a dynamic range of projects that demonstrate my growth as a versatile software engineer ready to make a meaningful impact in the tech industry."</p>
@@ -18,7 +17,6 @@ const Projects = () => {
                             <li className="li-contact"> Email - william.rogers.chi@gmail.com</li>
                             <li className="li-contact"> Socials - <Link className="li-link" to='https://github.com/williamrogerschi' target="_blank">GitHub </Link> <Link className="li-link" to='https://www.linkedin.com/in/billy-rogers-chi/' target="_blank">LinkedIn</Link></li>
                         </ul>
-                    </div>
                 </div>
                 <div className='work-list'>
                     <div className="work-container">
@@ -49,7 +47,7 @@ const Projects = () => {
                         <p>Tech stack used</p>
                         <p>takeaways</p>
                         </div>
-                </div>
+                        </div>
             </div>
             </>
     )
