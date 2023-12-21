9846763930ae624031580af7bb48938954e38a83
Billy Rogers <william.rogers.chi@gmail.com>
2023-12-21 15:47:21 -0600

    trying wihtout local storage for now

--- billy-port/src/App.jsx
+++ billy-port/src/App.jsx
@@ -2,21 +2,16 @@ import React, { createContext, useState } from 'react'
 import Main from './components/Main'
 import './App.css'
 import Toggle from './components/Toggle'
-import useLocalStorage from 'use-local-storage'
+// import useLocalStorage from 'use-local-storage'
 
 
 
 
 function App() {
 
-  if (typeof window !== 'undefined') {
-    console.log('Currently on Client side')
-} else {
-    console.log('Currently on Server Side');
-}
-
   const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches
-  const [isDark, setIsDark] = useLocalStorage('isDark', defaultDark)
+  // const [isDark, setIsDark] = useLocalStorage('isDark', defaultDark)
+  const [isDark, setIsDark] = useState('isDark', defaultDark)
 
 
   return (
