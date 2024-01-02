38f461444a85bbf3229dcf93925c53c456bc73e2
Billy Rogers <william.rogers.chi@gmail.com>
2024-01-02 11:19:14 -0600

    adding type animation and redirect to .toml file

--- billy-port/src/components/Contact.jsx
+++ billy-port/src/components/Contact.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 import HomeNav from "./HomeNav"
 import './contact.css'
+import  { TypeAnimation } from 'react-type-animation'
 
 
 
@@ -14,7 +15,24 @@ const Contact = () => {
                 <div className="contact-content">
                     <div className="contact-content-container">
                               <div className="hello">
-                                <h1 className="hello">Hello!</h1>
+                                <TypeAnimation
+                                    sequence={[
+                                        'Hello.',
+                                        1000,
+                                        '',
+                                        1000,
+                                        'Hello!',
+                                        () => {
+                                            console.log('Sequence completed')
+                                        },
+                                    ]}
+                                    speed={0.5}
+                                    wrapper='span'
+                                    cursor={true}
+                                    repeat={false}
+                                    style={{ fontSize: '1em', display: 'inline-block'}} 
+                                    />
+                                {/* <h1 className="hello">Hello!</h1> */}
                                 </div>  
                         <p className="about-p">A Software Engineer based out of Chicago, IL- looking to make my dreams become a reality!</p>
                         <ul >
