2e01e2ce226ab5071c640fb90bedec23fbf411fd
Billy Rogers <william.rogers.chi@gmail.com>
2024-01-04 10:50:31 -0600

    styling on projects and about page, adding cards

--- billy-port/src/components/About.jsx
+++ billy-port/src/components/About.jsx
@@ -5,15 +5,6 @@ import './about.css'
 
 const About = () => {
 
-    const [showAbout, setShowAbout] = useState(false)
-
-    useEffect(() => {
-        const timeout = setTimeout(() => {
-            setShowAbout(true)
-        }, 2000)
-
-        return () => clearTimeout(timeout)
-    }, [])
 
     return (
         <>
@@ -21,43 +12,48 @@ const About = () => {
             <div className="about-wrapper">
                 <div className="about-content">
                     <div className='about-img-wrapper'>
-                    <h1 className={`about ${showAbout ? 'visible' : ''}`}>About</h1>
-                    <div className='image-container'>
-                        <img className='headshot' src='./billy.jpeg' />
-                    </div>
+                        <h1 className='about-animation'>About</h1>
+                        <div className='image-container'>
+                            <img className='headshot' src='./billy.jpeg' />
+                        </div>
                     </div>
                     <div className='about-blurb-container'>
                         <h3 className="about-h3">Versatile software engineer with expertise in the MERN tech stack, complemented by a strong foundation in operations. Skilled in creating dynamic web applications and leveraging RESTful APIs. Key strengths include excellent interpersonal skills for fostering open and effective communication, a natural aptitude for problem-solving, and exceptional attention to detail. Demonstrated ability to thrive under pressure in challenging, multi-functional environments.</h3>
                     </div>
 
-                    <div className='experience-wrapper'>
-                        <div className='experience'>
-                            <p className='experience-header'>Experience</p>
-                        </div>
-                        <div className='experience-content'>
-                            <span className='about-exp'>- Certificate of completion for Software Engineering Immersive 12 week bootcamp from General Assembly  | </span> <span className='about-exp'>December 2023</span>
-                            <p className='content-exp'>- Degree in Business Administration from Northeastern Illinois University | Graduated May 2021</p>
-                        </div>
-                    </div>
+                    <div className='about-cards-wrapper'>
+                        <div className='about-cards'>
+                            <div className='experience-wrapper'>
+                                <div className='experience'>
+                                    <p className='experience-header'>Experience</p>
+                                </div>
+                                <div className='experience-content'>
+                                    <span className='about-exp'>- Certificate of completion for Software Engineering Immersive 12 week bootcamp from General Assembly  | </span> <span className='about-exp'>December 2023</span>
+                                    <p className='content-exp'>- Degree in Business Administration from Northeastern Illinois University | Graduated May 2021</p>
+                                </div>
+                            </div>
 
-                    <div className='resume-btn-wrapper'>
-                        <a href='https://drive.google.com/file/d/1N5YG8noAxEihHPyu8D1mGjXPN0CNLlCi/view?usp=sharing' target='_blank' className='resume-btn'>Resume</a>
-                    </div>
-
-                    <div className='experience-wrapper'>
-                        <div className='experience'>
-                            <p className='experience-header'>Skills</p>
+                            <div className='resume-btn-wrapper'>
+                                <a href='https://drive.google.com/file/d/1N5YG8noAxEihHPyu8D1mGjXPN0CNLlCi/view?usp=sharing' target='_blank' className='resume-btn'>Resume</a>
+                            </div>
                         </div>
-                        <div className='experience-content'>
-                            <span className='about-exp'>- Languages: </span> <span className='about-exp'>JavaScript ◍ HTML ◍ CSS ◍ Python ◍ SQL ◍ Mongo ◍ JSON | Database - MongoDB</span>
-                            <p className='content-exp'>- Libraries and Frameworks: React ◍ Express.js ◍ Django ◍  Bootstrap ◍ Tailwind</p>
-                            <p className='content-exp'>- Other: RESTful Routing ◍ JSON API  ◍  SAP ◍ Salesforce</p>
-                        </div>
-                    </div>
 
-                    <div className='resume-btn-wrapper'>
-                    <Link className='resume-btn' to='/Contact'>Contact</Link>
-                        {/* <a href='/Contact' className='resume-btn'>Contact</a> */}
+                        <div className='about-cards'>
+                            <div className='experience-wrapper'>
+                                <div className='experience'>
+                                    <p className='experience-header'>Skills</p>
+                                </div>
+                                <div className='experience-content'>
+                                    <span className='about-exp'>- Languages: </span> <span className='about-exp'>JavaScript ◍ HTML ◍ CSS ◍ Python ◍ SQL ◍ Mongo ◍ JSON | Database - MongoDB</span>
+                                    <p className='content-exp'>- Libraries and Frameworks: React ◍ Express.js ◍ Django ◍  Bootstrap ◍ Tailwind</p>
+                                    <p className='content-exp'>- Other: RESTful Routing ◍ JSON API  ◍  SAP ◍ Salesforce</p>
+                                </div>
+                            </div>
+
+                            <div className='resume-btn-wrapper'>
+                                <Link className='resume-btn' to='/Contact'>Contact</Link>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- billy-port/src/components/Work.jsx
+++ billy-port/src/components/Work.jsx
@@ -21,31 +21,35 @@ const Work = () => {
                 </div>
                 <div className='work-list'>
                     <div className="work-container">
-                        <a href="https://strava-api.netlify.app/" target='_blank' className="work-li">Strava API</a>
-                        {/* <img height={'300px'} width={'400px'} src="./swork.jpeg" /> */}
+                        <a href="https://strava-api.netlify.app/" target='_blank' className="work-li"><span className='title'>Strava API </span><span className='arrow'> ↪ </span></a>
+                        <div className='work-p-container'>
                         <p className='work-li-p'>Stats.Rides.Community.</p>
                         <p className='work-li-p'>Tech Stack: HTML ◍ CSS ◍ JavaScript</p>
+                        </div>
                         <p className='work-li-desc'>This was my first project and we had the choice between building a game or using a restful API. I chose to use an API and wanted to build an APP that would allow a user to login and check their profile, rides, and achievements in a simpler UI than what was currently being offered by Strava.</p>
                     </div>
                     <div className="work-container">
-                        <a href="#" className="work-li">Gwent CCG</a>
-                        {/* <img height={'300px'} width={'400px'} src="./swork.jpeg" /> */}
+                        <a href="#" className="work-li"><span className='title'>Gwent CCG </span><span className='arrow'> ↪ </span></a>
+                        <div className='work-p-container'>
                         <p className='work-li-p'>Gwent Custom Card Generator</p>
                         <p className='work-li-p'>Tech Stack: HTML ◍ CSS Javascript ◍ Mongoose ◍ Express</p>
+                        </div>
                         <p className='work-li-desc'>For my second project, I created a personalized card builder based off the CCG 'Gwent'. This project was all about making card customization a breeze for Gwent fans. I had alot of fun creating this, and quickly learned later on- that I would be able to streamline this using React.</p>
                     </div>
                     <div className="work-container">
-                        <a href="https://cocktail-party-ga.netlify.app/" target='_blank' className="work-li">Cocktail Party</a>
-                        {/* <img height={'300px'} width={'400px'} src="./swork.jpeg" /> */}
+                        <a href="https://cocktail-party-ga.netlify.app/" target='_blank' className="work-li"><span className='title'>Cocktail Party </span><span className='arrow'> ↪ </span></a>
+                        <div className='work-p-container'>
                         <p className='work-li-p'>What's a party without some cocktails and some recipes...</p>
                         <p className='work-li-p'>Tech Stack: MERN ◍ REST API</p>
+                        </div>
                         <p className='work-li-desc'>Cocktail Party, a hackathon project, uses React to tap into a cocktail recipe API. I focused on perfecting React mapping and rendering cards to create a user-friendly interface. The aim was simple: help users effortlessly discover cocktail recipes by browsing, searching, and filtering within the app. This project refined my API integration skills and showcased an easy-to-use platform for cocktail enthusiasts.</p>
                     </div>
                     <div className="work-container">
-                        <a href="https://bike-labs.netlify.app/" target='_blank' className="work-li">Bike Labs</a>
-                        {/* <img height={'300px'} width={'400px'} src="./swork.jpeg" /> */}
+                        <a href="https://bike-labs.netlify.app/" target='_blank' className="work-li"><span className='title'>Bike Labs </span><span className='arrow'> ↪ </span></a>
+                        <div className='work-p-container'>
                         <p className='work-li-p'>A bicycle created in `THE LAB`.</p>
                         <p className='work-li-p'>Tech Stack: MERN ◍  Bootstrap ◍ MongoDB</p>
+                        </div>
                         <p className='work-li-desc'>Imagine a platform where bike lovers can log in, check out different bike builds, and even create their own from scratch. I made it happen! With the MERN stack, I created an easy-to-use app that allowed users to log in, explore existing bike builds, and design their own with all the bells and whistles. Thjis was a hub for cyclists to discover, build, and share their passion for bikes. This project was my capstone and I showcased my skills for both frontend and backend magic. By having full CRUD across the app.</p>
                     </div>
                 </div>
