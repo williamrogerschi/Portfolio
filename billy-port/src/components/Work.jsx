428c3398a415268c1c048ec288305d253dc970e7
Billy Rogers <william.rogers.chi@gmail.com>
2024-01-04 12:57:43 -0600

    fixed arrows

--- billy-port/src/components/Work.jsx
+++ billy-port/src/components/Work.jsx
@@ -21,7 +21,7 @@ const Work = () => {
                 </div>
                 <div className='work-list'>
                     <div className="work-container">
-                        <a href="https://strava-api.netlify.app/" target='_blank' className="work-li"><span className='title'>Strava API </span><span className='arrow'> ↪ </span></a>
+                        <a href="https://strava-api.netlify.app/" target='_blank' className="work-li"><span className='title'>Strava API </span><span className='arrow'> 🡢 </span></a>
                         <div className='work-p-container'>
                         <p className='work-li-p'>Stats.Rides.Community.</p>
                         <p className='work-li-p'>Tech Stack: HTML ◍ CSS ◍ JavaScript</p>
@@ -29,7 +29,7 @@ const Work = () => {
                         <p className='work-li-desc'>This was my first project and we had the choice between building a game or using a restful API. I chose to use an API and wanted to build an APP that would allow a user to login and check their profile, rides, and achievements in a simpler UI than what was currently being offered by Strava.</p>
                     </div>
                     <div className="work-container">
-                        <a href="#" className="work-li"><span className='title'>Gwent CCG </span><span className='arrow'> ↪ </span></a>
+                        <a href="#" className="work-li"><span className='title'>Gwent CCG </span><span className='arrow'> 🡢 </span></a>
                         <div className='work-p-container'>
                         <p className='work-li-p'>Gwent Custom Card Generator</p>
                         <p className='work-li-p'>Tech Stack: HTML ◍ CSS ◍ Javascript ◍ Mongoose ◍ Express</p>
@@ -37,7 +37,7 @@ const Work = () => {
                         <p className='work-li-desc'>For my second project, I created a personalized card builder based off the CCG 'Gwent'. This project was all about making card customization a breeze for Gwent fans. I had alot of fun creating this, and quickly learned later on- that I would be able to streamline this using React.</p>
                     </div>
                     <div className="work-container">
-                        <a href="https://cocktail-party-ga.netlify.app/" target='_blank' className="work-li"><span className='title'>Cocktail Party </span><span className='arrow'> ↪ </span></a>
+                        <a href="https://cocktail-party-ga.netlify.app/" target='_blank' className="work-li"><span className='title'>Cocktail Party </span><span className='arrow'> 🡢 </span></a>
                         <div className='work-p-container'>
                         <p className='work-li-p'>What's a party without some cocktails and some recipes...</p>
                         <p className='work-li-p'>Tech Stack: MERN ◍ REST API</p>
@@ -45,7 +45,7 @@ const Work = () => {
                         <p className='work-li-desc'>Cocktail Party, a hackathon project, uses React to tap into a cocktail recipe API. I focused on perfecting React mapping and rendering cards to create a user-friendly interface. The aim was simple: help users effortlessly discover cocktail recipes by browsing, searching, and filtering within the app. This project refined my API integration skills and showcased an easy-to-use platform for cocktail enthusiasts.</p>
                     </div>
                     <div className="work-container">
-                        <a href="https://bike-labs.netlify.app/" target='_blank' className="work-li"><span className='title'>Bike Labs </span><span className='arrow'> ↪ </span></a>
+                        <a href="https://bike-labs.netlify.app/" target='_blank' className="work-li"><span className='title'>Bike Labs </span><span className='arrow'> 🡢 </span></a>
                         <div className='work-p-container'>
                         <p className='work-li-p'>A bicycle created in `THE LAB`.</p>
                         <p className='work-li-p'>Tech Stack: MERN ◍  Bootstrap ◍ MongoDB</p>
