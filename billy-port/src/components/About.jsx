1b7a639392b3e06dfa69c3a74c67d1e80d91fd65
Billy Rogers <william.rogers.chi@gmail.com>
2024-01-11 21:38:08 -0600

    updating projects cards slightly

--- billy-port/src/components/About.jsx
+++ billy-port/src/components/About.jsx
@@ -15,8 +15,7 @@ const About = () => {
                     <div className='about-all-container'>
                         <div className='about-img-wrapper'>
                         <img className='headshot' src='./billy.jpeg' />
-                            <h1 className='about-animation'>About</h1>
-  
+                            <div className='about-animation'>About</div>
                         </div>
                         <div className='about-blurb-container'>
                             <h3 className="about-h3">Versatile software engineer with expertise in the MERN tech stack, complemented by a strong foundation in operations. Skilled in creating dynamic web applications and leveraging RESTful APIs. Key strengths include excellent interpersonal skills for fostering open and effective communication, a natural aptitude for problem-solving, and exceptional attention to detail. Demonstrated ability to thrive under pressure in challenging, multi-functional environments.</h3>
@@ -31,8 +30,8 @@ const About = () => {
                                     <p className='experience-header'>Experience</p>
                                 </div>
                                 <div className='experience-content'>
-                                    <span className='about-exp'>- Certificate of completion for Software Engineering Immersive 12 week bootcamp from General Assembly  | </span> <span className='about-exp'>December 2023</span>
-                                    <p className='content-exp'>- Degree in Business Administration from Northeastern Illinois University | Graduated May 2021</p>
+                                    <span className='about-exp'> <span className="prof-span">Software Engineering Certificate</span> 12 week, 400+ hour intensive bootcamp at General Assembly  | </span> <span className='about-exp'>December 2023</span>
+                                    <p className='content-exp'> <span className="prof-span">Degree in Business Administration</span> from Northeastern Illinois University | May 2021</p>
                                 </div>
                             </div>
 
@@ -47,9 +46,9 @@ const About = () => {
                                     <p className='experience-header'>Skills</p>
                                 </div>
                                 <div className='experience-content'>
-                                    <span className='about-exp'>- Languages: </span> <span className='about-exp'>JavaScript ◍ HTML ◍ CSS ◍ Python ◍ SQL ◍ Mongo ◍ JSON | Database - MongoDB</span>
-                                    <p className='content-exp'>- Libraries and Frameworks: React ◍ Express.js ◍ Django ◍  Bootstrap ◍ Tailwind</p>
-                                    <p className='content-exp'>- Other: RESTful Routing ◍ JSON API  ◍  SAP ◍ Salesforce</p>
+                                    <span className='about-exp'> <span className="prof-span"> Languages: </span></span> <span className='about-exp'>JavaScript ◍ HTML ◍ CSS ◍ Python ◍ SQL ◍ Mongo ◍ JSON | Database - MongoDB</span>
+                                    <p className='content-exp'> <span className="prof-span">Libraries and Frameworks: </span> React ◍ Express.js ◍ Django ◍  Bootstrap ◍ Tailwind</p>
+                                    <p className='content-exp'> <span className="prof-span">Other: </span> RESTful Routing ◍ JSON API  ◍  SAP ◍ Salesforce</p>
                                 </div>
                             </div>
 
